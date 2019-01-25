2eb50846d85cb2aba8c9928b4748c2512964670b
Deepinder <deepinder@whitecircleschool.com>
2019-01-24 22:20:47 -0500

    Source Code

---
+++ JenkinsDemo/src/test/java/com/whitecircleschool/JenkinsDemo/VerifyTitle.java
@@ -0,0 +1,38 @@
+package com.whitecircleschool.JenkinsDemo;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class VerifyTitle {
+
+	WebDriver driver;
+
+	@BeforeTest
+	public void openBrowser() throws MalformedURLException {
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://facebook.com");
+	}
+
+	@Test
+	public void titleVerification() {
+		String expectedTitle = "Facebook – log in or sign up";
+		String actualTitle = driver.getTitle();
+		Assert.assertEquals(actualTitle, expectedTitle);
+
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.close();
+	}
+
+}
